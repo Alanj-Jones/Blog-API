96933321ede68245a1d1fcb0262a0c99f55fecce
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-05-19 18:50:15 -0300

    PostController: /posts view all posts method

--- src/main/java/com/challenge/warmup/controller/PostController.java
+++ src/main/java/com/challenge/warmup/controller/PostController.java
@@ -1,7 +1,13 @@
 package com.challenge.warmup.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.challenge.warmup.model.Post;
 import com.challenge.warmup.repository.PostRepository;
 import com.challenge.warmup.repository.UserRepository;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class PostController {
@@ -20,7 +27,13 @@ public class PostController {
     private UserRepository userRepo;
 
     @GetMapping("/posts")
-    public String viewPosts() {
+    public String viewPosts() {  
+        List<String> posts = new ArrayList<String>();      
+        for(Post p : postRepo.findAll()) {
+            posts.add(new Gson().toJson(p));   
+            System.out.println(p); //Debug        
+        }
+        System.out.println("Lista: " + posts.toString());
         return null;
     }
 
