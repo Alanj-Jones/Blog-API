8a95047d633c5988e6024d937e5031c8fb7598ce
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-05-20 03:04:02 -0300

    Delete user by id method created

--- src/main/java/com/challenge/warmup/controller/PostController.java
+++ src/main/java/com/challenge/warmup/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,8 +71,13 @@ public class PostController {
     }
 
     @DeleteMapping("/posts/{id}")
-    public String deletePost() {
-        return null;
+    public String deletePost(@PathVariable Integer id) {
+        try {
+            postRepo.deleteById(id);
+            return "Usuario borrado con exito";
+        } catch (Exception e) {
+            return e.getMessage();
+        }         
     }
 
 }
