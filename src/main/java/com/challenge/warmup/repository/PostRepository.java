0bf5112186a1a63991c811a8147cf1139e23757e
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-05-20 01:30:47 -0300

    PostRepository methods to filter Posts

--- src/main/java/com/challenge/warmup/repository/PostRepository.java
+++ src/main/java/com/challenge/warmup/repository/PostRepository.java
@@ -1,9 +1,16 @@
 package com.challenge.warmup.repository;
 
+import java.util.List;
+
 import com.challenge.warmup.model.Post;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Integer>{
     
+    List<Post> findByTitleLike(String title);
+    List<Post> findByCategoryLike(String category);
+    List<Post> findByTitleLikeAndCategoryLike(String title, String category);
+    
+
 }
