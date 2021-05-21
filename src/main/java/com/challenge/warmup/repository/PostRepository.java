630cdc69a0df278a1119fa25a479ae1667413407
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-05-21 00:17:39 -0300

    SQL query methods using nativeQuery

--- src/main/java/com/challenge/warmup/repository/PostRepository.java
+++ src/main/java/com/challenge/warmup/repository/PostRepository.java
@@ -6,12 +6,18 @@ import com.challenge.warmup.model.Post;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends CrudRepository<Post, Integer>{
-    
-    List<Post> findByTitleLike(String title);
-    List<Post> findByCategoryLike(String category);
-    List<Post> findByTitleLikeAndCategoryLike(String title, String category);
+
+    @Query(value = "SELECT post_id, title, image, category, creation_date FROM post WHERE title = :title ", nativeQuery = true)
+    List<Object> findByTitleLike(@Param("title") String title);
+
+    @Query(value = "SELECT post_id, title, image, category, creation_date FROM post WHERE category = :category", nativeQuery = true)
+    List<Object> findByCategoryLike(@Param("category") String category);
+
+    @Query(value = "SELECT post_id, title, image, category, creation_date FROM post WHERE title = :title and category = :category", nativeQuery = true)
+    List<Object> findByTitleLikeAndCategoryLike(@Param("title") String title, @Param("category") String category);
 
     @Query(value = "SELECT post_id, title, image, category, creation_date FROM post", nativeQuery = true)
     List<Object> getPosts();
